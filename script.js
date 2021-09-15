eb678fb4bb986070a62266fd5f629ac4aee09ba9
Valerii Bihun <valeriibihun.co@gmail.com>
2021-09-14 23:07:31 -0600

    fixed workability of "generatePassword()"

--- script.js
+++ script.js
@@ -1,67 +1,60 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialSymbols = ["!", "*", "}", "{", "[", "]", "/", "#", "%", "@", "(", ")", "_", ";", ":", "<", ">", "&", "?", "+", "`", "|", "-", "="];
 
-var finalPassword = [];
-
+var finalPassword = "";
 var lowercaseConfirmed = "";
 var uppercaseConfirmed = "";
 var numericConfirmed = "";
 var specialConfirmed = "";
-
+var passwordArray = [];
 
 var arrayPassword = function() {
   if (lowercaseConfirmed) {
-    finalPassword = finalPassword.concat(lowercase);
+    passwordArray = passwordArray.concat(letters);
   }
   if (uppercaseConfirmed){
-    finalPassword = finalPassword.concat(uppercase);
+    passwordArray = passwordArray.concat(bigLetters);
   }
   if (numericConfirmed){
-    finalPasswordConfirmed = finalPassword.concat(numeric);
+    passwordArray = passwordArray.concat(numbers);
   }
   if (specialConfirmed){
-    finalPassword = finalPassword.concat(special);
+    passwordArray = passwordArray.concat(specialSymbols);
   }
-  return finalPassword;
+  return passwordArray;
 
 };
 
 function randomChar(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
-}
+};
 
-function lengthPassword() {
-      var length = parseInt(prompt("Please, type in the lenght of your password (a number 8-128)","0"), 10);
+function generatePassword() {
+      var length = prompt("Please, type in the lenght of your password (a number from 8 to 128)");
       if (length > 128 ){
         alert("This isn`t a valid number! Please, input a number 8 - 126");
+        return;
       }
       if (length < 8 ){
         alert("This isn`t a valid number! Please, input a number 8 - 126");
+        return;
       }
-      else {
-        
-        generatePassword();
-      }
-      };
-
-function generatePassword() {
-      var lowercaseConfirmed = confirm("Would you like to include lowercase characters? ");
-      var uppercaseConfirmed = confirm("Would you like to include uppercase characters? ");
-      var numericConfirmed = confirm("Would you like to include numeric characters? ");
-      var specialConfirmed = confirm("Would you like to include srecial symbols? ");
-
+      lowercaseConfirmed = confirm("Would you like to include lowercase characters? ");
+      uppercaseConfirmed = confirm("Would you like to include uppercase characters? ");
+      numericConfirmed = confirm("Would you like to include numeric characters? ");
+      specialConfirmed = confirm("Would you like to include srecial symbols? ");
     if (lowercaseConfirmed === false && uppercaseConfirmed === false && numericConfirmed === false && specialConfirmed === false ) {
       alert("At least one option has to be selected!");
+      return;
     }
-    
     arrayPassword();
     for (i = 0; i <= length; i++) {
-      finalPassword = finalPassword.concat(randomChar(finalPassword));
+      finalPassword = finalPassword.concat(randomChar(passwordArray));
     }
     
     return finalPassword;
@@ -69,7 +62,7 @@ function generatePassword() {
       
 
 function writePassword() {
-  var finalPassword = lengthPassword();
+  var finalPassword = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = finalPassword;
   
